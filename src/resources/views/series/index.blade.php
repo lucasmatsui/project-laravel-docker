4b49f4db84613603ba7ed95319ab7a6d641ba84a
Lucas Kenzo <lucas.matsui@tray.net.br>
2019-11-06 15:54:59 -0200

    Adicionei a animaçao de editagem do nome da serie

--- src/resources/views/series/index.blade.php
+++ src/resources/views/series/index.blade.php
@@ -17,11 +17,28 @@
 <ul class="list-group">
   @foreach ($series as $serie)
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    {{ $serie->nome }}
+    <span id="nome-serie-{{ $serie->id }}">{{ $serie->nome }}</span>
+
+    <div class="input-group w-50" hidden id="input-nome-serie-{{ $serie->id }}">
+        <input type="text" class="form-control" value="{{ $serie->nome }}">
+        <div class="input-group-append">
+            <button class="btn btn-primary" onclick="editarSerie({{ $serie->id }})">
+                <i class="fas fa-check"></i>
+            </button>
+            @csrf
+        </div>
+    </div>
+
     <span class="d-flex">
+
+      <button class="btn btn-primary btn-sm mr-2" onclick="toogleInput({{ $serie->id }})">
+        <i class="fas fa-edit"></i>
+      </button>
+
       <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-info btn-sm mr-2">
         <i class="fas fa-external-link-alt"></i>
       </a>
+
       <form method="POST" action="/series/{{ $serie->id }}"
       onsubmit="return confirm('Tem certeza que deseja remover a série {{ addslashes($serie->nome) }} ?')">
         @csrf
@@ -35,4 +52,22 @@
   </li>
   @endforeach
 </ul>
+
+<script>
+  function toogleInput($serieId)
+  {
+    const nome = document.querySelector(`#nome-serie-${$serieId}`);
+    const input = document.querySelector(`#input-nome-serie-${$serieId}`);
+
+    if(input.hasAttribute('hidden')) {
+      nome.hidden = true;
+      input.removeAttribute('hidden');
+
+      return true;
+    }
+    nome.removeAttribute('hidden');
+    input.hidden = true;
+  }
+</script>
 @endsection
+
